a48acad293b115e98b219c5c92d77de6511964e6
brblacky <sdipedit@gmail.com>
2022-07-23 15:48:15 +0000

    discord.js v 14

---
+++ config.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+module.exports = {
+    token: process.env.TOKEN || "",  // your bot token
+    logs: process.env.LOGS || "", // channel id for lavalink server status logs
+    nodes: [
+        {
+            host: process.env.NODE_HOST || "",
+            identifier: process.env.NODE_ID || "",
+            port: parseInt(process.env.NODE_PORT || ""),
+            password: process.env.NODE_PASSWORD || "",
+            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+
+        }
+    ],
+
+}
+
+function parseBoolean(value) {
+    if (typeof (value) === 'string') {
+        value = value.trim().toLowerCase();
+    }
+    switch (value) {
+        case true:
+        case "true":
+            return true;
+        default:
+            return false;
+    }
+}
--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
-
-const { readdirSync } = require("fs");
+const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require("discord.js");
 const { Manager } = require("erela.js");
-const { token, nodes } = require('./config.json')
+const { nodes, token, logs } = require("./config.js");
 const client = new Client({
-    disableMentions: "everyone",
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    ws: { intents: Intents.ALL }
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildVoiceStates,
+
+    ],
+    partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
 });
 client.manager = new Manager({
-        nodes,
-        send: (id, payload) => {
+    nodes: nodes,
+    send: (id, payload) => {
         const guild = client.guilds.cache.get(id);
         if (guild) guild.shard.send(payload);
     },
-});
+}).on("nodeConnect", node => console.log(`Node "${node.options.identifier}" connected.`));
 
-  
-readdirSync("./Events/").forEach(file => {
-    const event = require(`./Events/${file}`);
-    let eventName = file.split(".")[0];
-    console.log(`Loading Events Client ${eventName}`, "event");
-    client.on(eventName, event.bind(null, client));
-});
+client.on("ready", async () => {
 
-  
+    const channel = await client.channels.fetch(logs);
+    const embed = new EmbedBuilder()
+        .setColor("#2F3136")
+        .setDescription("Please wait for a minute!\nStatus is being ready!")
+    channel.bulkDelete(10);
+    channel.send({ embeds: [embed] }).then((msg) => {
+        setInterval(() => {
+            let all = []
+            client.manager.nodes.forEach(node => {
+                let info = []
+                info.push(`Status: ${node.connected ? "🟢" : "🔴"}`)
+                info.push(`Node: ${(node.options.identifier)}`)
+                info.push(`Player: ${node.stats.players}`)
+                info.push(`Playing Players: ${node.stats.playingPlayers}`)
+                info.push(`Uptime: ${new Date(node.stats.uptime).toISOString().slice(11, 19)}`)
+                info.push("\nCPU")
+                info.push(`Cores: ${node.stats.cpu.cores}`)
+                info.push(`System Load: ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%`)
+                info.push(`Lavalink Load: ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`)
+                all.push(info.join('\n'))
+            });
+            const rembed = new EmbedBuilder()
+                .setAuthor({ name: 'Lavalink Node', iconURL: client.user.displayAvatarURL() })
+                .setURL(`https://discord.gg/ns8CTk9J3e`)
+                .setDescription(`\`\`\`${all.join('\n\n----------------------------\n')}\n\n` +
+                    `Total Memory  :: ${Math.round(require('os').totalmem() / 1024 / 1024)} mb\n` +
+                    `Free Memory   :: ${Math.round(require('os').freemem() / 1024 / 1024)} mb\n` +
+                    `RSS           :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
+                    `Heap Total    :: ${Math.round(process.memoryUsage().heapTotal / 1024 / 1024)} mb\n` +
+                    `Heap Used     :: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} mb\n` +
+                    `External      :: ${Math.round(process.memoryUsage().external / 1024 / 1024)} mb\n` +
+                    `Array Buffer  :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
+                    `CPU Model     :: ${require('os').cpus()[0].model}\n` +
+                    `Cores         :: ${require('os').cpus().length}\n` +
+                    `Speed         :: ${require('os').cpus()[0].speed}Mhz\n` +
+                    `Platform      :: ${process.platform}\n` +
+                    `PID           :: ${process.pid}\n` +
+                    `\n` + `\`\`\``)
+                .setColor("#9966ff")
+                .setTimestamp(Date.now());
+            msg.edit({ embeds: [rembed] });
+        }, 2000);
+    })
 
-  client.login(token);
+    client.manager.init(client.user.id);
+    console.log(`${client.user.username} online!`);
+
+})
+client.login(token);
+
